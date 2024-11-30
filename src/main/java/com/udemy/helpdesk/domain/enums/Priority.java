a7ce1306bfb8c3d93b8c8a7f2612a57814e1a18e
DionatanLeao <dionatan.ti@outlook.com>
2024-11-29 21:03:45 -0300

    feat: Create enumerated and insert lombok

---
+++ src/main/java/com/udemy/helpdesk/domain/enums/Priority.java
@@ -0,0 +1,27 @@
+package com.udemy.helpdesk.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Priority {
+    LOW(0, "LOW"),
+    MEDIUM(1, "MEDIUM"),
+    HIGH(2, "HIGH");
+
+    private Integer code;
+    private String description;
+
+    public static Priority toEnum(Integer code, String description) {
+        if (code == null) {
+            return null;
+        }
+        for (Priority x: Priority.values()) {
+            if (code.equals(x.getCode())) {
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Priority");
+    }
+}
