9f1b4b8d1b7c58305ecca464f3b75c6b874bfa43
DionatanLeao <dionatan.ti@outlook.com>
2024-12-04 22:07:49 -0300

    feat: Domain Classes Part 2

--- src/main/java/com/udemy/helpdesk/domain/enums/domain/Technician.java
+++ src/main/java/com/udemy/helpdesk/domain/enums/domain/Technician.java
@@ -1,19 +1,31 @@
 package com.udemy.helpdesk.domain.enums.domain;
 
+import com.udemy.helpdesk.domain.enums.Profile;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
+@Entity
 @Getter
 @Setter
 public class Technician extends Person {
+    private static final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "technician")
     private List<Ticket> tickets = new ArrayList<>();
+
+    public Technician() {
+        super();
+        addProfiles(Profile.TECHNICIAN);
+    }
 
     public Technician(Integer id, String name, String cpf, String email, String password, List<Ticket> tickets) {
         super(id, name, cpf, email, password);
+        addProfiles(Profile.TECHNICIAN);
     }
 }
--- src/main/java/com/udemy/helpdesk/domain/enums/domain/Ticket.java
+++ src/main/java/com/udemy/helpdesk/domain/enums/domain/Ticket.java
@@ -1,5 +1,6 @@
 package com.udemy.helpdesk.domain.enums.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.udemy.helpdesk.domain.enums.Priority;
 import com.udemy.helpdesk.domain.enums.Status;
 import lombok.EqualsAndHashCode;
@@ -7,21 +8,38 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 
+@Entity
 @NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Ticket {
+public class Ticket implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate openingDate = LocalDate.now();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate closingDate;
     private Priority priority;
     private Status status;
     private String title;
     private String observations;
+
+    @ManyToOne
+    @JoinColumn(name = "technician_id")
     private Technician technician;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
     private Client client;
 
     public Ticket(Integer id,
