501b090b8c32912df26721e79a639180b004816e
DionatanLeao <dionatan.ti@outlook.com>
2024-12-15 22:11:47 -0300

    feat: Implementing findAll for technician

--- src/main/java/com/udemy/helpdesk/resource/TechnicianResource.java
+++ src/main/java/com/udemy/helpdesk/resource/TechnicianResource.java
@@ -9,15 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/technicians")
 public class TechnicianResource {
     @Autowired
     private TechnicianService technicianService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<TechnicianDTO> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new TechnicianDTO(technicianService.findById(id)));
+    @GetMapping
+    public ResponseEntity<List<TechnicianDTO>> findAll() {
+        return ResponseEntity.ok()
+                .body(technicianService.findAll().stream().map(TechnicianDTO::new)
+                        .collect(Collectors.toList()));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<TechnicianDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok()
+                .body(new TechnicianDTO(technicianService.findById(id)));
+    }
 }
--- src/main/java/com/udemy/helpdesk/services/TechnicianService.java
+++ src/main/java/com/udemy/helpdesk/services/TechnicianService.java
@@ -6,10 +6,16 @@ import com.udemy.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TechnicianService {
     @Autowired
     private TechnicianRepository technicianRepository;
+
+    public List<Technician> findAll() {
+        return technicianRepository.findAll();
+    }
 
     public Technician findById(Integer id) {
         return technicianRepository.findById(id)
