34c41afa20f6d663e42c7e8da3108b8ebc1ec07e
DionatanLeao <dionatan.ti@outlook.com>
2024-12-23 23:51:28 -0300

    feat: Exception for CPF and E-mail already registered

--- src/main/java/com/udemy/helpdesk/repository/PersonRepository.java
+++ src/main/java/com/udemy/helpdesk/repository/PersonRepository.java
@@ -3,5 +3,9 @@ package com.udemy.helpdesk.repository;
 import com.udemy.helpdesk.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByCpf(String cpf);
+    Optional<Person> findByEmail(String email);
 }
